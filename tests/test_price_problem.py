742df4b1857ba98d9590ff3f517bbd231cd578b4
Alex Stefan <cenumesapunn@gmail.com>
2023-07-02 21:46:26 +0300

    test_price modified and added test_price_problem.py

--- tests/test_price_problem.py
+++ tests/test_price_problem.py
@@ -1,6 +1,7 @@
 import time
 
 import pytest
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 from TestData.Login import LoginData
@@ -10,7 +11,7 @@ from PageObjects.Login import Login
 
 class TestPrice(BaseClass):
 
-    @pytest.fixture(params=LoginData.test_StandardLogin_data)
+    @pytest.fixture(params=LoginData.test_Problem_Login_data)
     def get_data(self, request):
         return request.param
 
@@ -41,16 +42,36 @@ class TestPrice(BaseClass):
 
         log.info("##### 3. Price step from each item page and add to cart #####")
         list_price = []
+        price, item_price = None, None
         for item, index in enumerate(range(len(items))):
-            elements = self.driver.find_elements(By.XPATH, "//div[@class='inventory_item']")
-            elements[index].find_element(By.XPATH, ".//button[@class='btn btn_primary btn_small btn_inventory']").click()
-            element_name = elements[index].find_element(By.XPATH, ".//div[@class='inventory_item_name']").text
-            elements[index].find_element(By.XPATH, ".//a[@href='#']").click()
-            item_price = self.driver.find_element(By.XPATH, "//div[@class='inventory_details_price']").text
-            log.info(f"Element name = {element_name} and price {item_price}")
-            list_price.append(float(item_price[1::]))
-            self.driver.execute_script("window.history.go(-1)")
+            try:
+                elements = self.driver.find_elements(By.XPATH, "//div[@class='inventory_item']")
+                price = elements[index].find_element(By.XPATH, ".//div[@class='inventory_item_price']").text
+                price = float(price[1::])
+                elements[index].find_element(By.XPATH,
+                                             ".//button[@class='btn btn_primary btn_small btn_inventory']").click()
+                element_name = elements[index].find_element(By.XPATH, ".//div[@class='inventory_item_name']").text
+                elements[index].find_element(By.XPATH, ".//a[@href='#']").click()
+                item_price = self.driver.find_element(By.XPATH, "//div[@class='inventory_details_price']").text
+                log.info(f"Element name = {element_name} and price {item_price}")
+                log.info(f"Price from main page {price} vs, price on item page {item_price}")
+                list_price.append(float(item_price[1::]))
+                assert price == float(item_price[1::])
+                self.driver.execute_script("window.history.go(-1)")
+            except ValueError:
+                log.error("Error: Failed to convert price to float")
+                self.driver.execute_script("window.history.go(-1)")
+            except NoSuchElementException:
+                log.error("Error: Element not found")
+                self.driver.execute_script("window.history.go(-1)")
+            except AssertionError:
+                log.error(f"Assertion failed: Expected price {price}, Actual price {float(item_price[1:])}")
+                self.driver.execute_script("window.history.go(-1)")
         sum_list_price = sum(list_price)
 
-        assert price_sum == sum_list_price
-
+        try:
+            assert price_sum == sum_list_price
+            log.info(f"Price sum {price_sum} == sum_list price {sum_list_price}")
+        except AssertionError as e:
+            log.error(f"Assertion failed: Expected sum {price_sum}, Actual price {sum_list_price}")
+            raise e
