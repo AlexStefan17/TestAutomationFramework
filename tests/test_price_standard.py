03b98b3a938dd5cd18e186b18b7da08ba509f605
Alex Stefan <cenumesapunn@gmail.com>
2023-07-02 20:14:51 +0300

    added test_price

---
+++ tests/test_price_standard.py
@@ -0,0 +1,56 @@
+import time
+
+import pytest
+from selenium.webdriver.common.by import By
+
+from TestData.Login import LoginData
+from utilities.BaseClass import BaseClass
+from PageObjects.Login import Login
+
+
+class TestPrice(BaseClass):
+
+    @pytest.fixture(params=LoginData.test_StandardLogin_data)
+    def get_data(self, request):
+        return request.param
+
+    def test_price(self, get_data):
+        log = super().get_logger()
+
+        # Login step
+        log.info("##### 1. Login step #####")
+
+        login_page = Login(self.driver)
+        log.info(f"Username is {get_data['username']}")
+        login_page.get_username().send_keys(get_data["username"])
+        log.info(f"Password is {get_data['password']}")
+        login_page.get_password().send_keys(get_data["password"])
+        login_page.login_button().click()
+        url = super().get_url()
+        assert "https://www.saucedemo.com/inventory.html" == url, "Login failed"
+        log.info("Login is pass")
+
+        # Price step
+        log.info("##### 2. Price step from items page #####")
+
+        log.info("Calculate total price")
+        items = self.driver.find_elements(By.XPATH, "//div[@class='inventory_item']")
+        price_sum = sum(
+            [float(item.find_element(By.XPATH, ".//div[@class='inventory_item_price']").text[1:]) for item in items])
+        log.info(f"All elements price is {price_sum}")
+
+        log.info("##### 3. Price step from each item page and add to cart #####")
+        list_price = []
+        for item, index in enumerate(range(len(items))):
+            elements = self.driver.find_elements(By.XPATH, "//div[@class='inventory_item']")
+            elements[index].find_element(By.XPATH, ".//button[@class='btn btn_primary btn_small btn_inventory']").click()
+            element_name = elements[index].find_element(By.XPATH, ".//div[@class='inventory_item_name']").text
+            elements[index].find_element(By.XPATH, ".//a[@href='#']").click()
+            item_price = self.driver.find_element(By.XPATH, "//div[@class='inventory_details_price']").text
+            log.info(f"Element name = {element_name} and price {item_price}")
+            list_price.append(float(item_price[1::]))
+            self.driver.execute_script("window.history.go(-1)")
+        sum_list_price = sum(list_price)
+
+        assert price_sum == sum_list_price
+
